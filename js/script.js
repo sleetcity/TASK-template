297edd3b017e82f3301e4d2813487aa149f4c008
Shogo Higuchi <sleetcity@gmail.com>
2017-07-06 14:49:43 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+// お気に入りへ登録のボタン設置
+function addBookmark(title,url) {
+    if (window.sidebar) {
+        window.sidebar.addPanel(title, url,"");
+    } else if( document.all ) {
+        window.external.AddFavorite( url, title);
+    } else if( window.opera && window.print ) {
+        return true;
+    }
+}
+
+// ページトップへスクロール
+$(document).ready(function() {
+  var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+            }
+       });
+       pagetop.click(function () {
+           $('body, html').animate({ scrollTop: 0 }, 500);
+              return false;
+   });
+});
